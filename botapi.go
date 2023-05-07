e4d01f73d8c26bed67cc5decf36e97c164d7f50b
ghostik-gh <feodor200@mail.ru>
2023-05-07 19:01:18 +0300

    autoDelete

--- botapi.go
+++ botapi.go
@@ -9,6 +9,14 @@ import (
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 )
 
+const (
+	helpText = `Справочник по командам
+/set - Используется для установления пароля
+/get - Выводит список всех сервисов
+/del - Удаляет серевис из хранилища
+Бот обрабаывает только команды и пару слов после set, все остальное делается через кнопки под сообщениями`
+)
+
 func StartBot(config Config) {
 
 	bot, err := tgbotapi.NewBotAPI(config.TelegramBotToken)
@@ -23,6 +31,10 @@ func StartBot(config Config) {
 	updates := bot.GetUpdatesChan(u)
 	set := false
 	del := false
+	get := false
+	count := 0
+	lastMsg := 0
+	// inlineID := 0
 	for update := range updates {
 		// Обработка получения пароля не через аргументы
 		if del && update.Message != nil {
@@ -36,8 +48,14 @@ func StartBot(config Config) {
 			if err == nil {
 				msg.Text = "Удалил сервис: " + serv
 			}
+			if _, err := bot.Send(msg); err != nil {
+				fmt.Printf("err: %v\n", err)
+			}
+			count = 3
 			continue
 		}
+
+		// Обработка получения нового сервиса и пароля
 		if set && update.Message != nil {
 			set = false
 			msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
@@ -53,51 +71,52 @@ func StartBot(config Config) {
 			if _, err := bot.Send(msg); err != nil {
 				fmt.Printf("err: %v\n", err)
 			}
+			count = 3
 			continue
 		}
 
 		if update.Message != nil {
+			// if get {
+			// 	get = false
+			// 	msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
+			// 	edit := tgbotapi.NewEditMessageReplyMarkup(msg.ChatID, inlineID,
+			// 		tgbotapi.NewInlineKeyboardMarkup(tgbotapi.NewInlineKeyboardRow(tgbotapi.NewInlineKeyboardButtonData("Unavailable", "/get"))))
+			// 	if _, err := bot.Send(edit); err != nil {
+			// 		fmt.Printf("err: %v\n", err)
+			// 	}
+			// }
+			// lastMsg = Max(update.Message.MessageID, lastMsg)
+			if update.Message.MessageID >= lastMsg {
+				lastMsg = update.Message.MessageID
+				count = 1
+			}
 			msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
 			switch update.Message.Command() {
-			case "help":
-				msg.Text = `Справочник по командам
-/set - Используется для установления пароля
-/get - Выводит список всех сервисов
-/del - Удаляет серевис из хранилища
-Бот обрабаывает только команды и пару слов после set, все остальное делается через кнопки под сообщениями`
 			case "start":
-				msg.ReplyMarkup = Inline(update.Message.Chat.ID, update.Message.From.UserName)
-				msg.Text = `Для того чтобы увидеть пароль кликните на сервис
-Справочник по командам
-/set - Используется для установления пароля
-/get - Выводит список всех сервисов
-/del - Удаляет серевис из хранилища
-Бот обрабаывает только команды и пару слов после set, все остальное делается через кнопки под сообщениями`
+				msg.Text = helpText
+			case "help":
+				msg.Text = helpText
 			case "get":
+				count = 0
+				get = true
 				msg.ReplyMarkup = Inline(update.Message.Chat.ID, update.Message.From.UserName)
 				msg.Text = "Для того чтобы увидеть пароль кликните на сервис\nПароль удалится через 10 секунд"
 			case "del":
 				del = true
 				msg.Text = "Введите название сервиса"
-				// service, err := ValidateGet(update.Message.Text)
-				// if err != nil {
-				// 	fmt.Printf("err.Error(): %v\n", err)
-				// 	continue
-				// }
-				// err = DelPassword(update.Message.From.UserName, msg.ChatID, service)
-				// if err == nil {
-				// 	msg.Text = "Удалил сервис: " + service
-				// }
 			case "set":
 				set = true
 				msg.Text = "Напишите сервис и пароль к нему через пробел"
 			default:
 				msg.Text = "Я не знаю такой команды"
 			}
+			// Отправляем сообщение
 			if _, err := bot.Send(msg); err != nil {
 				fmt.Printf("err: %v\n", err)
 			}
 		} else if update.CallbackQuery != nil {
+			lastMsg = Max(update.CallbackQuery.Message.MessageID, lastMsg)
+			fmt.Printf("lastMsg: %v\n", lastMsg)
 			callback := tgbotapi.NewCallback(update.CallbackQuery.ID, "Пароль удаляется через 10 секунд")
 			if _, err := bot.Request(callback); err != nil {
 				panic(err)
@@ -107,16 +126,12 @@ func StartBot(config Config) {
 			if _, err := bot.Send(msg); err != nil {
 				panic(err)
 			}
-
-			go DeleteNextMsg(bot, msg.ChatID, update.CallbackQuery.Message.MessageID+1)
-			edit := tgbotapi.NewEditMessageReplyMarkup(msg.ChatID, update.CallbackQuery.Message.MessageID,
-				tgbotapi.NewInlineKeyboardMarkup(tgbotapi.NewInlineKeyboardRow(tgbotapi.NewInlineKeyboardButtonData("/get", "/get"))))
-			if _, err := bot.Send(edit); err != nil {
-				fmt.Printf("err: %v\n", err)
+			count++
+			if get {
+				go DeleteNextMsg(bot, msg.ChatID, lastMsg+count)
 			}
-
 		}
-
+		fmt.Printf("LAST count: %v\n", count)
 	}
 }
 
@@ -134,7 +149,7 @@ func Inline(chatID int64, username string) tgbotapi.InlineKeyboardMarkup {
 	}
 	var rows [][]tgbotapi.InlineKeyboardButton
 	for _, v := range data {
-		row := tgbotapi.NewInlineKeyboardRow(tgbotapi.NewInlineKeyboardButtonData(v.service, tgbotapi.EscapeText("MarkdownV2", "`"+v.password+"`")))
+		row := tgbotapi.NewInlineKeyboardRow(tgbotapi.NewInlineKeyboardButtonData(v.service, v.password))
 		rows = append(rows, row)
 	}
 	var numericKeyboard = tgbotapi.NewInlineKeyboardMarkup(rows...)
@@ -142,11 +157,18 @@ func Inline(chatID int64, username string) tgbotapi.InlineKeyboardMarkup {
 	return numericKeyboard
 }
 
+func Max(x, y int) int {
+	if x > y {
+		return x
+	}
+	return y
+}
+
 func DeleteNextMsg(bot *tgbotapi.BotAPI, chatID int64, MessageID int) {
 	del := tgbotapi.NewDeleteMessage(chatID, MessageID)
 	err := errors.New("msg does't not exist")
 	fmt.Printf("err: %v\n", err)
-	time.Sleep(10 * time.Second)
+	time.Sleep(2 * time.Second)
 	bot.Send(del)
 
 }
